766eb7fdc44a9ba75b83758fa1b649c420a34928
Claudio Cenghialta <62311968+claudiocenghialta@users.noreply.github.com>
2021-10-14 13:08:43 +0200

    step 2

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
 const app = express();
-app.use((req, res, next) => {
-  console.log('in the middleware');
-  next(); //Allows the request to continue to the next middleware in line
+app.use('/', (req, res, next) => {
+  console.log('this always run');
 })
-app.use((req, res, next) => {
-  console.log('in another middleware');
+app.use('/add-product',(req, res, next) => {
+  console.log('product middleware');
+  res.send('<h1>The add product page</h1>')
+})
+app.use('/',(req, res, next) => {
+  console.log('home middleware');
   res.send('<h1>Hello from express</h1>')
 })
 app.listen(3000);
